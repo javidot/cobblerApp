e60fb3ff4d9029ba5205ef8d02e747350e36374b
Javier Cintron <massiel.javier@gmail.com>
2017-08-10 12:44:33 -0400

    Fixing commit errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DndModule } from 'ng2-dnd';
 
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationModule } from './modules/navigation/navigation.module';
 import { TitleBarModule } from './modules/title-bar/title-bar.module';
 import { AppLibraryModule } from './modules/app-library/app-library.module';
+import { AppBuilderModule } from './modules/app-builder/app-builder.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
@@ -20,9 +23,13 @@ import { AppComponent } from './app.component';
     NavigationModule,
     TitleBarModule,
     AppLibraryModule,
+    AppBuilderModule,
+    SharedModule,
     NgbModule.forRoot(),
+    DndModule.forRoot(),
   ],
   exports: [
+    DndModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
